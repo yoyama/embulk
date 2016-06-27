cf13ae830fe409eff7b837bd479ce1854079cf73
You Yamagata <y.yamagata@astamuse.co.jp>
2016-06-27 18:20:16 +0900

    regex_conversions param in rename filter plugin.

--- embulk-standards/src/main/java/org/embulk/standards/RenameFilterPlugin.java
+++ embulk-standards/src/main/java/org/embulk/standards/RenameFilterPlugin.java
@@ -10,17 +10,33 @@ import org.embulk.spi.FilterPlugin;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.Schema;
 
+import java.util.List;
 import java.util.Map;
 
 public class RenameFilterPlugin
         implements FilterPlugin
 {
+
+    public interface RegexConversionParam
+            extends Task
+    {
+        @Config("regex")
+        String getRegex();
+
+        @Config("replacement")
+        String getReplacement();
+    }
+
     public interface PluginTask
             extends Task
     {
         @Config("columns")
         @ConfigDefault("{}")
         Map<String, String> getRenameMap();
+
+        @Config("regex_conversions")
+        @ConfigDefault("{}")
+        List<RegexConversionParam> getRegexConversions();
     }
 
     @Override
@@ -29,6 +45,7 @@ public class RenameFilterPlugin
     {
         PluginTask task = config.loadConfig(PluginTask.class);
         Map<String, String> renameMap = task.getRenameMap();
+        List<RegexConversionParam> regexConversions = task.getRegexConversions();
 
         // check column_options is valid or not
         for (String columnName : renameMap.keySet()) {
@@ -40,11 +57,23 @@ public class RenameFilterPlugin
             String name = column.getName();
             if (renameMap.containsKey(name)) {
                 name = renameMap.get(name);
+            } else {
+                name = getRegexConvertedName(name, regexConversions);
             }
             builder.add(name, column.getType());
         }
 
         control.run(task.dump(), builder.build());
+    }
+
+    protected String getRegexConvertedName(String orgColumn, List<RegexConversionParam> regexConversions) {
+        for(RegexConversionParam conv: regexConversions){
+            String newColumn = orgColumn.replaceAll(conv.getRegex(), conv.getReplacement());
+            if(!newColumn.equals(orgColumn)){
+                return newColumn;
+            }
+        }
+        return orgColumn;
     }
 
     @Override
